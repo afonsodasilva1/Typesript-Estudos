defeac8eb4943cd443ee09d27ab3e0f5bbdb62ff
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-28 01:32:17 +0100

    transpilador
    Usei o transpilador pela primeira vez

--- Firsts-Steps/main.ts
+++ Firsts-Steps/main.ts
@@ -1,11 +1,1 @@
-import mysql from 'mysql';
-
-const conexao = mysql.createConnection({
-host:'',
-user: '',
-password: '',
-port: '',
-database: ''
-});
-
-conexao.connect()
+console.log('Hello, World')
