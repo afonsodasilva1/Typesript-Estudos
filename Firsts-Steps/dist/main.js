5abb48ddf4b5b2b93d76995526a4f390716a1d54
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-28 09:45:20 +0100

    loop for
    I used loop for for the first time ever with typescript

---
+++ Firsts-Steps/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let nome = 'Carla';
+for (let index = 0; index < 10; index++) {
+    console.log(index + '- ' + nome);
+}
--- Firsts-Steps/main.ts
+++ Firsts-Steps/main.ts
@@ -1,1 +1,5 @@
-console.log('Hello, World')
+let nome = 'Carla'
+
+for (let index = 0; index < 10; index++) {
+    console.log(index+'- '+nome)
+}
