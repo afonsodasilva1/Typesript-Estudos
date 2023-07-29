81be07295b61a0e1cbb6e574f286fcb3c08429b1
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-29 20:49:59 +0100

    Update main.ts

--- Firsts-Steps/main.ts
+++ Firsts-Steps/main.ts
@@ -1,5 +1,15 @@
-let nome = 'Carla'
+//let aluno: [string, number, boolean]
+//let aluno: (string | number | boolean)[]
 
-for (let index = 0; index < 10; index++) {
-    console.log(index+'- '+nome)
+//aluno = ['Afonso', 19, 'true']
+
+//let objecto: Record<string, number | string>
+//let objecto: {id:number; selecao: string; grupo: string}
+
+//objecto = {id: 1, selecao: 'Brasil', grupo: 'G'}
+
+function soma(num1: number, num2: number) {
+    return num1 + num2
 }
+
+const soma2 = (a: number, b: number) => a + b;
